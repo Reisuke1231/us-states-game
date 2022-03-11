7c78f354c8e9f0a22c3066643937de8df5561037
Reisuke1231 <reisuke1231@gmail.com>
2022-03-11 16:49:07 +0900

    Implement untile display state when answer is right

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title('U.S. States Game')
 image = 'blank_states_img.gif'
 screen.addshape(image)
-
 turtle.shape(image)
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+data = pandas.read_csv('50_states.csv')
+guessed_states = []
 
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title=f'{len(guessed_states)}/50 Guess the States',
+        prompt='What\'s another state\'s name?'
+    ).title()
 
+    if answer_state in data.state.to_list():
+        label = turtle.Turtle()
+        label.hideturtle()
+        label.penup()
+        state_data = data[data['state'] == answer_state]
+
+        label.goto(int(state_data.x), int(state_data.y))
+        label.write(state_data.state.item())
+
+        guessed_states.append(answer_state)
 
 screen.exitonclick()
